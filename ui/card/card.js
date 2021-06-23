0313e9fd8cab158890e876ae8f815af6cb75e4cf
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2021-06-23 11:41:41 -0300

    Adding a function to add a background id

--- ui/card/card.js
+++ ui/card/card.js
@@ -3,26 +3,38 @@ import { rgba } from 'polished'
 
 import { Tag, GradientText, EventProps } from 'ui'
 
-export const Card = ({ eventName, eventDate, eventHour, eventLinks }) => (
-  <Wrapper>
-    <Title>{eventName}</Title>
-    <Description>{eventDate} <GradientText>•</GradientText> {eventHour}</Description>
-    <TagContainer>
-      {eventLinks.map(({ name, url }, index) => (
-        <Tag name={name} url={url} key={`${name}-${index}`} />
-      ))}
-    </TagContainer>
-  </Wrapper>
-)
+export const Card = ({ backgroundId, eventName, eventDate, eventHour, eventLinks }) => {
+  const background = backgrounds[backgroundId]
+  return (
+    <Wrapper background={background}>
+      <Title>{eventName}</Title>
+      <Description>
+        {eventDate} <GradientText>•</GradientText> {eventHour}
+      </Description>
+      <TagContainer>
+        {eventLinks.map(({ name, url }, index) => (
+          <Tag name={name} url={url} key={`${name}-${index}`} />
+        ))}
+      </TagContainer>
+    </Wrapper>
+  )
+}
 
 Card.propTypes = EventProps
+
+const backgrounds = [
+  { url: './card-bg-1.svg', position: 'right 6.4rem bottom 1.2rem' },
+  { url: './card-bg-2.svg', position: 'right 1.2rem top 1.2rem' },
+  { url: './card-bg-3.svg', position: 'right 1.2rem top 1.2rem' },
+  { url: './card-bg-4.svg', position: 'right 2.4rem bottom 1.2rem' },
+]
 
 const Wrapper = styled.article`
   padding: 4rem 4.6rem 5rem;
   border-radius: 0.8rem;
-  background-image: url('./card-bg.svg');
+  background-image: url(${({ background }) => background.url});
   background-repeat: no-repeat;
-  background-position: bottom right;
+  background-position: ${({ background }) => background.position};
   background-color: ${({ theme }) => rgba(theme.colors.text, 0.05)};
 `
 
