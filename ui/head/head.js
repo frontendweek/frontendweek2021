636ef53fd5d0bcfa05cf8d5520bb75e9990ae8d2
elvisdetona <sergiowebdesigner@gmail.com>
2021-07-01 16:48:25 -0300

    share metatag improvements

--- ui/head/head.js
+++ ui/head/head.js
@@ -7,17 +7,21 @@ const HeadComponent = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-
-      <meta itemProp="name" content="Front-end Week 2021 &bull; 8ª Edição" />
-      <meta itemProp="image" content="/og-600x600.png" />
-      <meta itemProp="url" content="http://frontendweek.com.br/" />
-      <meta itemProp="description" content="Confira a programação da 8ª edição da Front-End Week." />
+      <meta name="description" content="Confira a programação da 8ª edição da Front-End Week." />
+      <meta name="format-detection" content="telephone=no" />
 
       <meta property="og:title" content="Front-end Week 2021 &bull; 8ª Edição" />
-      <meta property="og:image" content="/og-240x240.png" />
+      <meta property="og:image" content="/frontendweek-share.png" />
       <meta property="og:url" content="http://frontendweek.com.br/" />
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Front-end Week" />
       <meta property="og:description" content="Confira a programação da 8ª edição da Front-End Week." />
+
+      <meta name="twitter:title" content="Front-end Week 2021 &bull; 8ª Edição" />
+      <meta name="twitter:description" content="Confira a programação da 8ª edição da Front-End Week." />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="/frontendweek-share.png" />
+      <meta name="twitter:site" content="@frontendweek" />
 
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-TileImage" content="/favicon-32x32.png" />
